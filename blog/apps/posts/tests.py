dde2703e19612c039ee7ee3b39cc9ce2d47f50b9
Herman Caldara <hermancaldara@gmail.com>
2011-01-15 05:42:21 -0200

    Criado teste para a função que retorna todas as tags de todos os posts

--- blog/apps/posts/tests.py
+++ blog/apps/posts/tests.py
@@ -1,23 +1,20 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from apps.posts.models import Post
+from should_dsl import should
+from utils import pegar_todas_tags
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TesteTags(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def setUp(self):
+        self.primeiro_post = Post.objects.create()
+        self.segundo_post = Post.objects.create()
+        self.terceiro_post = Post.objects.create()
+        
+    def teste_pegar_todas_tags(self):
+        self.primeiro_post.tags.set('python', 'django')
+        self.segundo_post.tags.set('ruby', 'rails')
+        self.terceiro_post.tags.set('php', 'miolo')
+        pegar_todas_tags() |should| equal_to(
+            ['django', 'miolo', 'php', 'python', 'rails', 'ruby']
+       )
